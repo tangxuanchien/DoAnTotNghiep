11e6b8a260e4cf1f81d5ada815909dd8f92a1bf8
tangxuanchien <tangxuanchien511@gmail.com>
2024-09-10 14:09:27 +0700

    first commit

--- controllers/update.controller.php
+++ controllers/update.controller.php
@@ -2,6 +2,7 @@
 $body = $_POST['body'];
 $id = $_GET['id'];
 require '../models/Database.php';
+require
 $db = new Database();
 $todo = $db->query("UPDATE `notes` SET body='$body' WHERE id = $id")->fetch(PDO::FETCH_ASSOC);
 
